dbdfd1ce1d11a4a1e7c2a9fb656b32061bf6b783
Leon San José Larsson <leonlarsson8@gmail.com>
2022-02-06 01:20:26 +0100

    ES6 it up

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ async function fetchImage() {
         .then(response => {
             json0 = response;
         });
-        
-        await fetch("https://www.reddit.com/r/raccoons/hot.json?limit=100") // Fetch posts
+
+    await fetch("https://www.reddit.com/r/raccoons/hot.json?limit=100") // Fetch posts
         .then(response => response.json())
         .then(response => {
             json1 = response;
@@ -19,16 +19,16 @@ async function fetchImage() {
 
 function loadImage() {
 
-    source = Math.floor(Math.random() * 2); // Gets 0 or 1
-    response = window["json" + source]; // json0 or json1
+    const source = Math.floor(Math.random() * 2); // Gets 0 or 1
+    const response = window[`json${source}`]; // json0 or json1
 
 
     document.getElementById("text").style.display = "none";
     document.getElementById("vidText").style.display = "none";
-    child = Math.floor(Math.random() * 100);
+    const child = Math.floor(Math.random() * 100);
     const images = ["i.redd.it", ".jpg", ".png", "jpeg"];
     const videos = ["v.redd.it"];
-    const youtube = ["youtube", "youtu.be"]
+    const youtube = ["youtube", "youtu.be"];
     const gfycat = ["gfycat"];
     const gifv = [".gifv"];
 
@@ -39,25 +39,23 @@ function loadImage() {
         const imgUrl = response.data.children[child].data.url; // Gets post URL
 
         if (images.some(i => imgUrl.includes(i))) { // If post is image
-            console.log("----------------------------------------\n[DEBUG] Found image: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
+            console.log(`----------------------------------------\n[DEBUG] Found image: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
 
             document.getElementById("video").setAttribute("src", " ");
             document.getElementById("youtube").setAttribute("src", " ");
-            document.getElementById("image").setAttribute("src", imgUrl)
+            document.getElementById("image").setAttribute("src", imgUrl);
             document.getElementById("image").style.display = "block";
             document.getElementById("video").style.display = "none";
             document.getElementById("youtube").style.display = "none";
 
         } else if (videos.some(i => imgUrl.includes(i))) { // If post is video
-            // console.log("----------------------------------------\n[DEBUG] Found video: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
-
             if (response.data.children[child].data.hasOwnProperty("crosspost_parent")) {
                 subredditCrosspost = response.data.children[child].data.crosspost_parent_list[0].subreddit;
-                console.log("----------------------------------------\n[DEBUG] Found video: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit} from r/${subredditCrosspost})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
+                console.log(`----------------------------------------\n[DEBUG] Found video: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit} from r/${subredditCrosspost})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
                 const videoURL = response.data.children[child].data.crosspost_parent_list[0].media.reddit_video.fallback_url;
                 document.getElementById("video").setAttribute("src", videoURL);
             } else {
-                console.log("----------------------------------------\n[DEBUG] Found video: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
+                console.log(`----------------------------------------\n[DEBUG] Found video: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
                 const videoURL = response.data.children[child].data.media.reddit_video.fallback_url;
                 document.getElementById("video").setAttribute("src", videoURL);
             }
@@ -71,24 +69,22 @@ function loadImage() {
             return;
 
         } else if (youtube.some(i => imgUrl.includes(i))) { // If post is youtube video
-            console.log("----------------------------------------\n[DEBUG] Found YouTube: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
-            response.data.children[child].data.secure_media_embed.media_domain_url
-            var newStr = response.data.children[child].data.secure_media.oembed.thumbnail_url.slice(23, -14);
-            youtubeUrl = "https://www.youtube.com/embed/" + newStr + "?autoplay=1&mute=1&feature=oembed&enablejsapi=1";
+            console.log(`----------------------------------------\n[DEBUG] Found YouTube: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
+            const newStr = response.data.children[child].data.secure_media.oembed.thumbnail_url.slice(23, -14);
+            const youtubeUrl = `https://www.youtube.com/embed/${newStr}?autoplay=1&mute=1&feature=oembed&enablejsapi=1`;
 
             document.getElementById("youtube").setAttribute("src", youtubeUrl);
             document.getElementById("youtube").style.display = "block";
             document.getElementById("video").setAttribute("src", " ");
-            document.getElementById("image").setAttribute("src", " ")
+            document.getElementById("image").setAttribute("src", " ");
             document.getElementById("image").style.display = "none";
             document.getElementById("video").style.display = "none";
             return;
 
         } else if (gfycat.some(i => imgUrl.includes(i))) { // If post is gfycat
-            console.log("----------------------------------------\n[DEBUG] Found gfycat: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
-
-            var newStr = response.data.children[child].data.secure_media.oembed.thumbnail_url.slice(26, -20); // Get the gif name with the correct capitalizations
-            gfycatUrl = "https://giant.gfycat.com/" + newStr + ".mp4" // Build the Gfycat mp4 url
+            console.log(`----------------------------------------\n[DEBUG] Found gfycat: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
+            const newStr = response.data.children[child].data.secure_media.oembed.thumbnail_url.slice(26, -20); // Get the gif name with the correct capitalizations
+            const gfycatUrl = `https://giant.gfycat.com/${newStr}.mp4` // Build the Gfycat mp4 url
 
             document.getElementById("image").setAttribute("src", " ");
             document.getElementById("youtube").setAttribute("src", " ");
@@ -99,10 +95,9 @@ function loadImage() {
             document.getElementById("video").play();
             return;
         } else if (gifv.some(i => imgUrl.includes(i))) { // If post is gifv
-            console.log("----------------------------------------\n[DEBUG] Found gifv: " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
-
-            var newStr = response.data.children[child].data.url.slice(0, -5); // remove ".gif"
-            gifvUrl = newStr + ".mp4" // Build the gifv mp4 url
+            console.log(`----------------------------------------\n[DEBUG] Found gifv: ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
+            const newStr = response.data.children[child].data.url.slice(0, -5); // remove ".gif"
+            const gifvUrl = `${newStr}.mp4`; // Build the gifv mp4 url
 
             document.getElementById("image").setAttribute("src", " ");
             document.getElementById("youtube").setAttribute("src", " ");
@@ -114,12 +109,13 @@ function loadImage() {
             return;
 
         } else {
-            console.log("----------------------------------------\n[DEBUG] Found unrecognized format (re-running): " + imgUrl + " from https://reddit.com/" + linkID + ` (r/${subreddit})\n[DEBUG] Post number: ` + child + "\n----------------------------------------");
+            console.log(`----------------------------------------\n[DEBUG] Found unrecognized format (re-running): ${imgUrl} from https://reddit.com/${linkID} (r/${subreddit})\n[DEBUG] Post number: ${child}\n----------------------------------------`);
             loadImage();
         }
+
     } catch (e) {
         document.getElementById("vidText").style.display = "none";
-        console.log(`%c[DEBUG] (${source}, ${child}) Something went wrong or I found a crosspost. Re-running.\n\n[ERROR] ` + e, `color: #dc5b4a`)
+        console.log(`%c[DEBUG] (${source}, ${child}) Something went wrong or I found a crosspost. Re-running.\n\n[ERROR] ${e}`, "color: #dc5b4a");
         loadImage();
     }
 }
